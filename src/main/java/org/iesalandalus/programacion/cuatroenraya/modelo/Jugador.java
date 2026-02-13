7f6222725c81c306a559c83e24d4fab7fcf9c00a
Manuel_mma <manumaral04@gmail.com>
2026-02-13 18:54:56 +0100

    Registro Jugador Terminado y JugadorTest superado

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -4,29 +4,29 @@ import java.util.Objects;
 
 public record Jugador(String nombre, Ficha colorFichas) {
 
-public Jugador{
-validarNombre(nombre);
-validarColorFichas(colorFichas);
+    public Jugador {
+        validarNombre(nombre);
+        validarColorFichas(colorFichas);
 
-}
-
-private void validarNombre(String nombre)  {
-    Objects.requireNonNull(nombre,"El nombre no puede ser nulo.");
-
-    if (nombre.isBlank()){
-        throw new IllegalArgumentException("El nombre no puede estar en blanco.");
     }
 
-}
+    private void validarNombre(String nombre) {
+        Objects.requireNonNull(nombre, "El nombre no puede ser nulo.");
 
-private void validarColorFichas(Ficha colorFichas){
+        if (nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar en blanco.");
+        }
 
-    Objects.requireNonNull(colorFichas,"El color de las fichas no puede ser nulo.");
+    }
 
-}
+    private void validarColorFichas(Ficha colorFichas) {
+
+        Objects.requireNonNull(colorFichas, "El color de las fichas no puede ser nulo.");
+
+    }
 
     @Override
     public String toString() {
-        return String.format("%s (%s)", nombre, colorFichas.name().substring(0,1));
+        return String.format("%s (%s)", nombre, colorFichas.name().substring(0, 1));
     }
 }
