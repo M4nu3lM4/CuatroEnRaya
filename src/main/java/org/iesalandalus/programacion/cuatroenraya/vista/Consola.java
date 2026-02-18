6adb08b1604126b522f3b8f44a5b68de125d5501
Manuel_mma <manumaral04@gmail.com>
2026-02-18 13:46:19 +0100

    Clase Consola terminada

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -7,15 +7,16 @@ import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
 
-    private Consola(){}
+    private Consola() {
+    }
 
-    public static String leerNombre(){
+    public static String leerNombre() {
 
         String nombre;
         do {
             System.out.print("Introduce el nombre del jugador: ");
             nombre = Entrada.cadena();
-        }while (nombre.isEmpty());
+        } while (nombre.isEmpty());
         return nombre;
     }
 
@@ -24,7 +25,7 @@ public class Consola {
         int opcion;
         System.out.print("Elige el color de tus fichas( 0-AZUL, 1-VERDE): ");
         opcion = Entrada.entero();
-        switch (opcion){
+        switch (opcion) {
             case 0 -> colorFichas = Ficha.AZUL;
             case 1 -> colorFichas = Ficha.VERDE;
             default -> throw new CuatroEnRayaExcepcion("Opción no válida.");
@@ -40,7 +41,7 @@ public class Consola {
         leerNombre();
         elegirColorFichas();
 
-        return new Jugador(leerNombre(),elegirColorFichas());
+        return new Jugador(leerNombre(), elegirColorFichas());
     }
 
     public static Jugador leerJugador(Ficha ficha) throws CuatroEnRayaExcepcion {
@@ -48,15 +49,15 @@ public class Consola {
         System.out.print("Introduce los datos del SEGUNDO jugador ");
         leerNombre();
         leerJugador(ficha);
-        return new Jugador(leerNombre(),elegirColorFichas());
+        return new Jugador(leerNombre(), elegirColorFichas());
     }
 
-    public static int leerColumna(Jugador jugador){
+    public static int leerColumna(Jugador jugador) {
         int columna;
         do {
-            System.out.print(jugador.nombre()+ "introduce la columna en la que deseas introducir la ficha (0 - 6): ");
+            System.out.print(jugador.nombre() + "introduce la columna en la que deseas introducir la ficha (0 - 6): ");
             columna = Entrada.entero();
-        }while (columna < 0 || columna > 6);
+        } while (columna < 0 || columna > 6);
 
         return columna;
 
