5abe61716dbacfaae785e9da09a8a5be681d3a6f
Manuel_mma <manumaral04@gmail.com>
2026-02-24 14:20:46 +0100

    Main Terminado

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/Main.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/Main.java
@@ -8,10 +8,20 @@ public class Main {
 	
 	public static void main(String[] args) {
 
+        try {
+            Jugador jugador1 = Consola.leerJugador();
 
+            Ficha fichaContraria = (jugador1.colorFichas() == Ficha.AZUL) ? Ficha.VERDE : Ficha.AZUL;
 
+            Jugador jugador2 = Consola.leerJugador(fichaContraria);
 
+            CuatroEnRaya cuatroEnRaya = new CuatroEnRaya(jugador1, jugador2);
 
-	}
-	
+            cuatroEnRaya.jugar();
+
+        } catch (CuatroEnRayaExcepcion e) {
+            System.out.println("ERROR:"+ e.getMessage());
+        }
+    }
+
 }
